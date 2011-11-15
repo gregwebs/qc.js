06cae355115baf7c47ebd7fce0d1eb16a82de6b6
Wolfram Kriesing <cain@horst.local>
2011-11-15 18:05:46 +0100

    Tiny english fix.

--- src/core.js
+++ src/core.js
@@ -17,36 +17,19 @@ define([
   }
 
   /**
-   * draws a new value from a generator.
-   * A generator is either a function accepting a seed argument or an object
-   * with a method 'arb' accepting a seed argument.
-   *
-   * @param gen Function or Generator object with method 'arb'
-   * @param {Number} size seed argument
-   *
-   * @return new generated value
-   */
-  exports.genvalue = function(gen, size) {
-      if (!(gen instanceof Function)) {
-          gen = gen.arb;
-      }
-      return gen(size);
-  }
-
-  /**
    * Uses the generators specific shrinking method to shrink a value the
    * generator created before. If the generator is a function or has no method
    * named 'shrink' or the objects field 'shrink' is set to null, no shrinking
    * will be done.  If a shrinking method is defined, this method is called with
    * the original seed and value the generator created. The shrinking method is
-   * supposed to return an Array of shrinked(!) values or null,undefined,[] if
-   * no shrinked values could have been created.
+   * supposed to return an Array of shrunk(!) values or null,undefined,[] if
+   * no shrunk values could have been created.
    *
    * @param gen the generator object
    * @param size the initial seed used when creating a value
    * @param arg the value the generator created for testing
    *
-   * @return an array of shrinked values or [] if no shrinked values were
+   * @return an array of shrunk values or [] if no shrunk values were
    *         generated.
    *
    */
@@ -65,35 +48,35 @@ define([
    * @private
    */
   exports.shrinkLoop = function(config, prop, size, args) {
-      var loop, i, testCase, failedArgs = [args], shrinkedArgs = [];
+      var loop, i, testCase, failedArgs = [args], shrunkArgs = [];
 
       for (loop = 0; loop < config.maxShrink; loop++) {
           // create shrinked argument lists from failed arguments
 
-          shrinkedArgs = [];
+          shrunkArgs = [];
           for (i = 0; i < failedArgs.length; i++) {
-              shrinkedArgs = shrinkedArgs.concat(
-                  prop.generateShrinkedArgs(size, failedArgs[i]));
+              shrunkArgs = shrunkArgs.concat(
+                  prop.generateshrunkArgs(size, failedArgs[i]));
           }
 
-          if (shrinkedArgs.length === 0) {
+          if (shrunkArgs.length === 0) {
               return failedArgs.length === 0 ? null : failedArgs[0];
           }
 
           // create new failed arguments from shrinked ones by running the
           // property
           failedArgs = [];
-          for (i = 0; i < shrinkedArgs.length; i++) {
+          for (i = 0; i < shrunkArgs.length; i++) {
               try {
-                  testCase = new Case(shrinkedArgs[i]);
-                  prop.body.apply(prop, [testCase].concat(shrinkedArgs[i]));
+                  testCase = new Case(shrunkArgs[i]);
+                  prop.body.apply(prop, [testCase].concat(shrunkArgs[i]));
               } catch (e) {
                   if (e === 'InvalidCase') {
                   } else if (e === 'AssertFailed') {
                       if (loop === config.maxShrink - 1) {
-                          return shrinkedArgs[i];
+                          return shrunkArgs[i];
                       } else {
-                          failedArgs.push(shrinkedArgs[i]);
+                          failedArgs.push(shrunkArgs[i]);
                       }
                   } else {
                       throw e;
