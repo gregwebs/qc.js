da7f4ad92ff5c2d9a22b1636a828683e0a88ec7f
Wolfram Kriesing <cain@localhost.(none)>
2011-11-16 22:39:18 +0100

    Added a couple more tests.

--- test/generator-base.js
+++ test/generator-base.js
@@ -10,22 +10,69 @@ define([
 
   var consts = [1, 'one', 'uno'];
   qc.declare("constants", [gen.constants.apply(null, consts)],
-    function(c, a) {
-      c.assert(consts.indexOf(a) != -1);
+    function(testCase, value) {
+      testCase.assert(consts.indexOf(value) != -1);
     }
   );
 
   qc.declare(
     "choose from (positive) integers",
     [gen.choose(gen.number.integers, gen.number.positiveIntegers)],
-    function(c, a) {
-      c.assert(parseInt(a)==a);
+    function(testCase, value) {
+      testCase.assert(parseInt(value)==value);
     }
   );
 
   qc.declare("booleans", [gen.booleans],
-    function(c, a) {
-      c.assert(a===true || a===false);
+    function(testCase, value) {
+      testCase.assert(value===true || value===false);
+    }
+  );
+
+  qc.declare("nulls", [gen.nulls],
+    function(testCase, value) {
+      testCase.assert(value===null);
+    }
+  );
+
+  var allTypes = [
+    gen.number.integers, gen.number.floats, gen.number.positiveIntegers,
+    gen.string.strings, gen.string.chararcters,
+    gen.booleans, gen.nulls, gen.dates, gen.undefineds
+  ];
+  qc.declare("arrays", [gen.arrays(gen.choose.apply(null, allTypes))],
+    function(testCase, value) {
+      testCase.assert(Object.prototype.toString.call(value) == '[object Array]');
+    }
+  );
+
+//  qc.declare("non empty arrays", [gen.nonEmptyArrays],
+//    function(testCase, value) {
+//      testCase.assert(0);
+//    }
+//  );
+//
+  qc.declare("dates", [gen.dates],
+    function(testCase, value) {
+      testCase.assert(value instanceof Date);
+    }
+  );
+
+  qc.declare("null or ...", [gen.nullOr(gen.number.integers)],
+    function(testCase, value) {
+      testCase.assert(value===null || typeof value == 'number');
+    }
+  );
+
+  qc.declare("undefineds", [gen.undefineds],
+    function(testCase, value) {
+      testCase.assert(typeof value == 'undefined');
+    }
+  );
+
+  qc.declare("undefined or ...", [gen.undefinedOr(gen.number.integers)],
+    function(testCase, value) {
+      testCase.assert(typeof value == 'undefined' || typeof value == 'number');
     }
   );
 
@@ -34,13 +81,13 @@ define([
 /*
   qc.declare("random positive integer", [random.getPositiveInteger],
     function(c, a) {
-      c.assert(!isNaN(a) && parseInt(a)===a && a >= 0);
+      testCase.assert(!isNaN(a) && parseInt(a)===a && a >= 0);
     }
   );
 
   qc.declare("random float", [random.getFloat],
     function(c, a) {
-      c.assert(!isNaN(a));
+      testCase.assert(!isNaN(a));
     }
   );
 //*/
