33d1665098a57e13eb53eb9638a808f984d0fbd5
Wolfram Kriesing <cain@horst.local>
2011-11-17 09:15:30 +0100

    Test coverage for all positive cases, shrink not yet included.

--- test/generator-number.js
+++ test/generator-number.js
@@ -2,17 +2,29 @@ define([
   'qc'
 ],function(qc){
 
-  var gen = qc.generator;
+  var gen = qc.generator.number;
 
-  qc.declare("strings", [gen.string.strings],
+  qc.declare("integers", [gen.integers],
     function(testCase, value) {
-      testCase.assert(value === ('' + value));
+      testCase.assert(parseInt(value) === value);
     }
   );
 
-  qc.declare("characters", [gen.string.chararcters],
+  qc.declare("positive integers", [gen.positiveIntegers],
     function(testCase, value) {
-      testCase.assert(value === ('' + value));
+      testCase.assert(parseInt(value) === value && value >= 0);
+    }
+  );
+
+  qc.declare("floats", [gen.floats],
+    function(testCase, value) {
+      testCase.assert(parseFloat(value) === value);
+    }
+  );
+
+  qc.declare("range", [gen.range(-10, 10)],
+    function(testCase, value) {
+      testCase.assert(value >= -10 && value <= 10);
     }
   );
 
