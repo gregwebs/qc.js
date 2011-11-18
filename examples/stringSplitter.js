9d0804a3e1d0a1fae178fff56f085f5c16feff48
Wolfram Kriesing <cain@localhost.(none)>
2011-11-18 11:01:23 +0100

    Quite some commenting.

--- examples/stringSplitter.js
+++ examples/stringSplitter.js
@@ -18,14 +18,23 @@ define([
 
   var gen = qc.generator;
 
+  // Here we create the function that we pass to our test.
+  // This function creates an non-empty array of strings, which will be
+  // our words that shall be joined and split.
   var words = gen.nonEmptyArrays(gen.string.strings);
 
+  // Define the test property (that's what its called in the quickcheck world).
+  // The following is the function that will be executed a defined number of times (100 in our case),
+  // see runner.html (the Config() constructor). For each run words
+  // is called and uses the above generators (nonEmptyArrays and strings)
+  // to create random values.
   qc.declare("stringSplitter", [words],
     function(testCase, value) {
       var possibleSeparators = getDistinctCharacters(value.join(''));
       testCase.guard(possibleSeparators.length != 0); // If no separator can be found, try again, marks this test case as invalid.
       var separator = possibleSeparators[0];
       var input = value.join(separator);
+      // The actual test, that the split function works :).
       var splitResult = input.split(separator);
       // Compare the string representation of the arrays.
       testCase.assert( ''+value == ''+splitResult );
@@ -34,6 +43,10 @@ define([
 
 
 
+  /**
+   * Return an array of characters that are not contained in the
+   * given ones.
+   */
   var getDistinctCharacters = function(chars){
     var unused = [];
     for (var i=32; unused.push(i) && i<255; i++){}; // Create an array with all ints from 32..255, all possible chars.
