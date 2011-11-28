ee4445c4e88298caff508008a6e0575bc400e574
Wolfram Kriesing <wk@uxebu.com>
2011-11-28 23:56:39 +0100

    Added __all__.js also for html module.

---
+++ src/generator/html/__all__.js
@@ -0,0 +1,12 @@
+define('generator/html/__all__', [
+  'generator/html/color'
+], function(color) {
+
+  // Populate the following using their namespaces.
+  // So they will be reachable via 'qs.generator.html'
+  var exports = {
+    color: color
+  };
+
+  return exports;
+});
