fa7721a67a7756b02c698b131143f55b4f8cd867
Wolfram Kriesing <wk@uxebu.com>
2011-12-06 00:01:12 +0200

    Simplified listener, more explicit output ... still in the works.

--- src/HtmlListener.js
+++ src/HtmlListener.js
@@ -8,25 +8,26 @@ define('HtmlListener', [
    * @extends ConsoleListener
    * @class
    */
-  function HtmlListener(nodeId, showPasses) {
+  function HtmlListener(params) {
     this.maxCollected = 0;
-    this._showPasses = showPasses;
-    this._domNode = document.getElementById(nodeId);
+    this._showPassedTests = params.showPassedTests || true;
+    this._domNode = document.getElementById(params.nodeId);
   }
   HtmlListener.prototype = new ConsoleListener();
   HtmlListener.prototype.passed = function (str) {
-    if (this._showPasses) {
-      this._domNode.innerHTML += str.status + ': ' + str.name + ' -- ' + JSON.stringify(str.stats) + '<br>';
+    if (this._showPassedTests) {
+//      this._domNode.innerHTML += str.status + ': ' + str.name + ' -- ' + JSON.stringify(str.stats) + '<br>';
+      this._domNode.innerHTML += str + '<br>';
     }
   };
   HtmlListener.prototype.invalid = function (str) {
-    this._domNode.innerHTML += str;
+    this._domNode.innerHTML += str + '<br>';
   };
   HtmlListener.prototype.failure = function (str) {
     this._domNode.innerHTML += str + '<br>';
   };
   HtmlListener.prototype.log = function (str) {
-    this._domNode.innerHTML += str;
+    this._domNode.innerHTML += str + '<br>';
   };
   HtmlListener.prototype.done = function (str) {
     this._domNode.innerHTML += 'DONE.';
