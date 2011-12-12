34a4e64dd14ca924266c8d4e9b215952f6fa8e05
Wolfram Kriesing <wk@uxebu.com>
2011-12-12 15:45:11 +0100

    Forgot to deactivate todo tests.

--- test/generator-number.js
+++ test/generator-number.js
@@ -72,32 +72,32 @@
       }
     );
     
-    qc.declare("floatRanges with precision 0.1", [gen.floatRanges(-10, 10, 1)],
-      function(testCase, value) {
-        testCase.assert(value >= -10 && value <= 10);
-        // Verify precision.
-        var parts = (''+value).split('.');
-        if(parseInt(value) == value){
-          testCase.assert(true);
-        } else {
-          testCase.assert(parts[1].length<=1);
-        }
-      }
-    );
-
-    qc.declare("floatRanges with precision 0.01", [gen.floatRanges(-10, 10, 2)],
-      function(testCase, value) {
-        testCase.assert(value >= -10 && value <= 10);
-        // Verify precision.
-        var parts = (''+value).split('.');
-        if(parseInt(value) == value){
-          testCase.assert(true);
-        } else {
-          testCase.assert(parts[1].length<=2);
-        }
-      }
-    );
-
+//    qc.declare("floatRanges with precision 0.1", [gen.floatRanges(-10, 10, 1)],
+//      function(testCase, value) {
+//        testCase.assert(value >= -10 && value <= 10);
+//        // Verify precision.
+//        var parts = (''+value).split('.');
+//        if(parseInt(value) == value){
+//          testCase.assert(true);
+//        } else {
+//          testCase.assert(parts[1].length<=1);
+//        }
+//      }
+//    );
+//
+//    qc.declare("floatRanges with precision 0.01", [gen.floatRanges(-10, 10, 2)],
+//      function(testCase, value) {
+//        testCase.assert(value >= -10 && value <= 10);
+//        // Verify precision.
+//        var parts = (''+value).split('.');
+//        if(parseInt(value) == value){
+//          testCase.assert(true);
+//        } else {
+//          testCase.assert(parts[1].length<=2);
+//        }
+//      }
+//    );
+//
     qc.declare("floatRanges with random ranges", [gen.floats(), gen.floats()],
       function(testCase, minValue, maxValue) {
         var min = Math.min(minValue, maxValue);
